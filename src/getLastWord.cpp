8a2117344eaa7e97d2d4e9ab16487a31672ebf4f
Saikrishna-K <Saikrishna-K@missionrnd.com>
2016-03-18 16:26:36 +0530

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -12,5 +12,46 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 
 char * get_last_word(char * str){
 
-	return NULL;
+	if (str == NULL) return NULL; // if the string is a null string
+
+	int i, j = 0, sIndex = 0, lIndex = 0;
+		
+
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' &&j == 0)// checking for  the initial index of the string
+		{
+			j = 1;
+			sIndex = i;
+
+		}
+		else if (str[i] == ' '  && j == 1)// checking for the end index of the string if there are spaces after the end word
+		{
+			j = 0;
+			lIndex = i;
+		}
+
+		if (str[i] != ' ' && str[i + 1] == '\0' && j == 1)// checking for the end index of the string if the EOF is'\0'
+		{
+			lIndex = i;
+		}
+
+	}
+
+	if (j == 0)     // if the given string is an empty string returning empty string
+		return "";
+
+
+	char *last = (char *)calloc(lIndex - sIndex + 2, sizeof(char));
+	j = 0;
+	for (i = sIndex; i <= lIndex; i++)
+	{
+		last[j++] = str[i];
+
+	}
+
+	last[j] = '\0';
+
+	return last;
 }
